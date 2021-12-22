f86e455de6471c5b5edeeb48e1e2216889f95d1e
polo <paulguzdariviere@gmail.com>
2021-12-22 19:17:59 +0100

    Add nodejs/npm cmds checkings

--- vue-sandbox/install.sh
+++ vue-sandbox/install.sh
@@ -2,9 +2,15 @@
 
 echo "Starting installation of VueSandbox tool..."
 
-# Check for Git command
+# Check for required commands (git, node, npm)
 command -v git >/dev/null 2>&1 || {
   echo >&2 "'git' could not be found. Please install it first: sudo apt install git"; exit 1;
+}
+command -v nodejs >/dev/null 2>&1 || {
+  echo >&2 "'nodejs' could not be found. Please install it first: sudo apt install git"; exit 1;
+}
+command -v npm >/dev/null 2>&1 || {
+  echo >&2 "'npm' could not be found. Please install it first: sudo apt install git"; exit 1;
 }
 
 # Package installation
