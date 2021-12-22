434fe8e49316813955c4fd4b0dd11982e1080957
polo <paulguzdariviere@gmail.com>
2021-12-22 19:14:02 +0100

    Set installation process for VS tool

--- vue-sandbox/install.sh
+++ vue-sandbox/install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-echo "Shell script is working."
+echo "Starting installation of VueSandbox tool..."
+
+# Check for Git command
+command -v git >/dev/null 2>&1 || {
+  echo >&2 "'git' could not be found. Please install it first: sudo apt install git"; exit 1;
+}
+
+# Package installation
+pkg_repo="git@github.com:mekkanix/mekkanix.github.io.git"
+git clone $pkg_repo
