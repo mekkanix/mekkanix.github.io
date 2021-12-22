f72777280ff8f2208b14cdaeb171b8ed5865a4c5
polo <paulguzdariviere@gmail.com>
2021-12-22 22:36:25 +0100

    Clean VS install script & add call to VS's install script

--- vue-sandbox/install.sh
+++ vue-sandbox/install.sh
@@ -1,18 +1,35 @@
 #!/bin/sh
 
-echo "Starting installation of VueSandbox tool..."
+# Vars
+pkg_repo="git@github.com:mekkanix/vue-sandbox.git"
+pkg_version="1.0.0-beta.1"
+
+echo "+--------------------------------------+"
+echo "| VueSandbox installer - v$pkg_version |"
+echo "+--------------------------------------+"
+
+echo "> Starting installation of VueSandbox..."
 
 # Check for required commands (git, node, npm)
 command -v git >/dev/null 2>&1 || {
-  echo >&2 "'git' could not be found. Please install it first: sudo apt install git"; exit 1;
+  echo >&2 "> 'git' is missing: installing...";
+  sudo apt install git
 }
 command -v nodejs >/dev/null 2>&1 || {
-  echo >&2 "'nodejs' could not be found. Please install it first: sudo apt install git"; exit 1;
+  echo >&2 "> 'nodejs' is missing: installing...";
+  sudo apt install nodejs
 }
 command -v npm >/dev/null 2>&1 || {
-  echo >&2 "'npm' could not be found. Please install it first: sudo apt install git"; exit 1;
+  echo >&2 "> 'npm' is missing: installing...";
+  sudo apt install npm
 }
 
 # Package installation
-pkg_repo="git@github.com:mekkanix/mekkanix.github.io.git"
 git clone $pkg_repo
+
+cd vue-sandbox/
+
+echo "---"
+echo "> Building..."
+
+sh ./install.sh
